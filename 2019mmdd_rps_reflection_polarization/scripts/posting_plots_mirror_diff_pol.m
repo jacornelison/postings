04ccb9ddcaaaf8fbaf00026697c60ab1a10fbb32
jcornelison <james_cornelison@g.harvard.edu>
2021-05-28 11:18:09 -0400

    prettier diff pol tile plots

--- 2019mmdd_rps_reflection_polarization/scripts/posting_plots_mirror_diff_pol.m
+++ 2019mmdd_rps_reflection_polarization/scripts/posting_plots_mirror_diff_pol.m
@@ -6,7 +6,7 @@ n = 2400;
 k = 2400;
 dk = -180:45:135;
 
-[phipair,phia,phib,Ainc] = calc_mirror_diff_pol(p,n,k,dk);
+[phipair,phia,phib,Ainc,epspair] = calc_mirror_diff_pol(p,n,k,dk);
 
 %%
 scaling = 0.000002;
@@ -31,7 +31,7 @@ plot_tiles(mean(phipair,2),p,'fig',1,'clim',[-1,1]*scaling,'title',['mean'],'pai
 
 
  %%
-    scaling = 0.005;
+    scaling = 0.04;
 for dkind = 1:length(dk)
     figure(1); clf;
     plot_tiles(phia(:,dkind),p,'fig',1,'clim',[-1,1]*scaling,'title',['dk ' num2str(dk(dkind))],'pair','mean','clab','\phi_a');
@@ -51,7 +51,7 @@ plot_tiles(mean(phia,2),p,'fig',1,'clim',[-1,1]*scaling,'title',['mean'],'pair',
     fname = sprintf('../figs/tileplot_phia_dk_mean_rps');
     saveas(1,fname,'png')
 
-scaling = 0.005;
+
 for dkind = 1:length(dk)
     figure(1); clf;
     plot_tiles(phib(:,dkind)-90,p,'fig',1,'clim',[-1,1]*scaling,'title',['dk ' num2str(dk(dkind))],'pair','mean','clab','\phi_b-90');
