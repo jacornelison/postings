b6f4f89cda069ca145e33d6892fcbf892e7c1406
jcornelison <james_cornelison@g.harvard.edu>
2023-01-26 19:08:12 -0500

    so many updates. Mostly differential polarization

--- 2019mmdd_rps_reflection_polarization/scripts/posting_plots_mirror_diff_pol.m
+++ 2019mmdd_rps_reflection_polarization/scripts/posting_plots_mirror_diff_pol.m
@@ -1,20 +1,22 @@
 function posting_plots_mirror_diff_pol()
 
+clear all
+close all
 postdir = fullfile('C:','Users','James','Documents','GitHub','postings','2019mmdd_rps_reflection_polarization','');
 datadir = fullfile(postdir,'data','');
 figdir = fullfile(postdir,'figs','');
 %load(fullfile(datadir,'b3rpsfiles.mat'))
 load('z:/dev/rps/fpu_data_obs.mat')
-n = 2400;
-k = 2400;
-dk = -180:45:135;
+n = 2200;
+k = 2200;
+dk = -180:45:180;
 
 [phipair,phia,phib,Ainc,epspair] = calc_mirror_diff_pol(p,n,k,dk);
 
 fig = figure(1);
 fig.Position(3:4) = [900 700];
 
-%
+%%
 scaling = 0.000002;
 names = {'phipair','phia','phib','epspair'};
 vals = {phipair,phia,phib-90,epspair};
@@ -48,49 +50,150 @@ plot_tiles(mean(vals{valind},2),p,'fig',1,'clim',[-1,1]*scales(valind),'title','
     saveas(1,fname,'png')
 
 end
- %%
-    scaling = 0.04;
-for dkind = 1:length(dk)
-    figure(1); clf;
-    plot_tiles(phia(:,dkind),p,'fig',1,'clim',[-1,1]*scaling,'title',['dk ' num2str(dk(dkind))],'pair','mean','clab','\phi_a');
-    colormap jet
-    fname = sprintf('tileplot_phia_dk_%i',dk(dkind));
-    fname = fullfile(figdir,fname);
-    saveas(1,fname,'png')
+%% Scatter plot of phia/b/pair vs DK
+clc
+fig = figure(1);
+fig.Position(3:4) = [900 800];
+clf;
+
+p_dummy = struct;
+p_dummy.r = [0; 7; 15];
+p_dummy.theta = zeros(size(p_dummy.r));
+p_dummy.drumangle = zeros(size(p_dummy.r));
+p_dummy.chi = zeros(size(p_dummy.r));
+p_dummy.chi_thetaref=zeros(size(p_dummy.r));
+
+
+[phipair,phia,phib,Ainc,epspair] = calc_mirror_diff_pol(p_dummy,n,k,dk);
+
+V0 = {phia, phib-90, phipair};
+ylims = {[-1 1]*1, [-1 1]*1, [-1 1]*0.5};
+yttls = {'\phi_0','\phi_{90}','\phi_{pair}'};
+for valind = 1:length(V0)
+    K = repmat(dk,length(p_dummy.r),1);
+    K = reshape(K,[],1);
+    clr = repmat(p_dummy.r,1,length(dk));
+    clr = reshape(clr,[],1);
+    V = reshape(V0{valind},[],1);
+    subplot(3,1,valind)
+    scatter(K,V,14,clr,'filled')
+    grid on
+    colormap('turbo')
+    ylabel([yttls{valind} ' [Degrees]'])
+    %ylim(ylims{valind})
+
 end
+xlabel('DK angle [Degrees]')
 
-plot_tiles(mean(phia,2),p,'fig',1,'clim',[-1,1]*scaling,'title',['mean'],'pair','mean','clab','\phi_a');
-    colormap jet
-    fname = sprintf('tileplot_phia_dk_mean');
-    fname = fullfile(figdir,fname);
-    saveas(1,fname,'png')
+%% Plot the reflection coefficients
 
+clc
+fig = figure(11);
+fig.Position(3:4) = [450 400];
+clf; hold on;
+
+Nair = 1;
+Nal = 2200*(1+sqrt(-1));
+
+theta_i = 0:65;
+Atx = sqrt(1-(Nair/Nal*sind(theta_i)).^2);
+Rs = abs((Nair*cosd(theta_i)-Nal*Atx)./(Nair*cosd(theta_i)+Nal*Atx)).^2;
+Rp = abs((Nair*Atx-Nal*cosd(theta_i))./(Nair*Atx+Nal*cosd(theta_i))).^2;
+
+plot(theta_i,Rs,'b')
+plot(theta_i,Rp,'r')
+grid on
+title('Reflectivity of Aluminum @ 90GHz, 240 Kelvin')
+xlabel('\theta_{inc} [Degrees]')
+ylabel('Reflectivity')
+legend({'S (Perpendicular)','P (Parallel)'},'location','southwest')
+fname = 'al_reflectivity.png';
+saveas(fig,fullfile(figdir,fname),'png')
+
+%% Plot mirror diff pol
+fig = figure(1);
+clf; hold on;
+
+clc
+rs = [0 7 14];
+DK = -180:45:180;
+phi_rot = NaN(length(DK),2);
+phi0 = {0 90};
+ylabs = {'\phi','\phi','\phi_{pair}'};
+ttls = {'Input of 0 degrees','Input of 90 Degrees','Pair-Diff Angle'};
+for phiind = 1:(length(phi0)+1)
+    subplot(3,1,phiind)
+    hold on
+for rind = rs
+    if phiind ~=3
+    phi_in = zeros(size(DK))+phi0{phiind};
+    r = zeros(size(DK))+rind;
+    theta = zeros(size(DK));
+    mtilt = zeros(size(DK))+45;
+    mroll = zeros(size(DK));
     
-    plot_tiles(mean(phia(:,1:4),2),p,'fig',1,'clim',[-1,1]*scaling,'title',['mean 0-135'],'pair','mean','clab','\phi_a');
-    colormap jet
-    fname = sprintf('tileplot_phia_dk_mean_rps');
-    fname = fullfile(figdir,fname);
-    saveas(1,fname,'png')
-
-
-for dkind = 1:length(dk)
-    figure(1); clf;
-    plot_tiles(phib(:,dkind)-90,p,'fig',1,'clim',[-1,1]*scaling,'title',['dk ' num2str(dk(dkind))],'pair','mean','clab','\phi_b-90');
-    colormap jet
-    fname = sprintf('tileplot_phib_dk_%i',dk(dkind));
-    fname = fullfile(figdir,fname);
-    saveas(1,fname,'png')
+    V = mirror_diff_pol_calc(phi_in,r,theta,2200,2200,DK,mtilt,mroll);
+    %V = mirror_diff_pol_correction(V,r,theta,2400,2400,DK,mtilt,mroll);
+    phi_rot(:,phiind) = V;
+    else
+        V = (phi_rot(:,1)+phi_rot(:,2)-90)/2;
+    end
+    plot(DK,V)
+    
 end
+legend({'r = 0deg' 'r = 7deg' 'r = 14deg'})
+grid on
+title(ttls{phiind})
+    ylabel([ylabs{phiind} ' [Degrees]'])
 
-plot_tiles(mean(phib,2)-90,p,'fig',1,'clim',[-1,1]*scaling,'title',['mean'],'pair','mean','clab','\phi_b-90');
-    colormap jet
-    fname = sprintf('tileplot_phib_dk_mean');
-    fname = fullfile(figdir,fname);
-    saveas(1,fname,'png')
+end
+xlabel('DK [Degrees]')
+fname = 'phi_bias.png';
+saveas(fig,fullfile(figdir,fname),'png')
 
+
+%% Plot Correction
+fig = figure(1);
+clf; hold on;
+
+clc
+rs = [0 7 14];
+DK = -180:45:180;
+phi_rot = NaN(length(DK),2);
+phi0 = {0 90};
+ylabs = {'\phi','\phi','\phi_{pair}'};
+ttls = {'Input of 0 degrees','Input of 90 Degrees','Pair-Diff Angle'};
+for phiind = 1:(length(phi0)+1)
+    subplot(3,1,phiind)
+    hold on
+for rind = rs
+    if phiind ~=3
+    phi_in = zeros(size(DK))+phi0{phiind};
+    r = zeros(size(DK))+rind;
+    theta = zeros(size(DK));
+    mtilt = zeros(size(DK))+45;
+    mroll = zeros(size(DK));
     
-    plot_tiles(mean(phib(:,1:4),2)-90,p,'fig',1,'clim',[-1,1]*scaling,'title',['mean 0-135'],'pair','mean','clab','\phi_b-90');
-    colormap jet
-    fname = sprintf('tileplot_phib_dk_mean_rps');
-    fname = fullfile(figdir,fname);
-    saveas(1,fname,'png')
+    V = mirror_diff_pol_calc(phi_in,r,theta,2200,2200,DK,mtilt,mroll);
+    V = mirror_diff_pol_correction(V,r,theta,2200,2200,DK,mtilt,mroll);
+    phi_rot(:,phiind) = V;
+    else
+        V = (phi_rot(:,1)+phi_rot(:,2)-90)/2;
+    end
+    plot(DK,V)
+    
+end
+legend({'r = 0deg' 'r = 7deg' 'r = 14deg'})
+grid on
+title(ttls{phiind})
+    ylabel([ylabs{phiind} ' [Degrees]'])
+
+end
+xlabel('DK [Degrees]')
+fname = 'phi_correction.png';
+saveas(fig,fullfile(figdir,fname),'png')
+
+
+
+
+
--- 2022mmdd_rps_angles_xpol/scripts/posting_plots_rps_analysis_2.m
+++ 2022mmdd_rps_angles_xpol/scripts/posting_plots_rps_analysis_2.m
@@ -4,6 +4,10 @@ function posting_plots_rps_analysis_2()
 clear all
 close all
 
+% Load no tilt data
+load('z:/dev/rps/rps_beam_fits_type5_notilt_cut.mat')
+fd_notilt = fd;
+
 % Load Type 9 data
 % Load type 9 data
 load('z:/dev/rps/type9_fit_dat_mirrorfitted_cut.mat')
@@ -22,12 +26,22 @@ load('z:/dev/rps/rps_beam_fits_type5_final_cut')
 load('z:/dev/rps/fpu_data_obs.mat')
 load('z:/pipeline/beammap/viridis_cm.mat')
 load('z:/dev/sims/coaddopt_6600.mat')
+load('z:/dev/rps/sch_type5.mat')
 p_ind = coaddopt.ind;
 figdir = fullfile('C:','Users','James','Documents','GitHub','postings','2022mmdd_rps_angles_xpol','figs','');
 
 cmlines = colormap('lines');
-%% Grab the chis
 
+[fd.theta, fd.r] = pol2cart(fd.x,fd.y);
+fd.theta = fd.theta*180/pi;
+
+fd.phi_corr = mirror_diff_pol_calc(fd.phi,fd.r,fd.theta,2132,2132,fd.dk_cen,fd.mirr_tilt,fd.mirr_roll);
+fd.phi_corr = reshape(fd.phi_corr,size(fd.ch));
+
+
+
+% If 1 use chflags from B18
+if 1
 inda = p_ind.rgl100a;
 indb = p_ind.rgl100b;
 
@@ -36,7 +50,19 @@ ind0 = [p_ind.rgl100a(ismember(p_ind.rgl100a,find(p.mce~=0))) ...
 ind90 = [p_ind.rgl100b(ismember(p_ind.rgl100b,find(p.mce~=0))) ...
     p_ind.rgl100a(ismember(p_ind.rgl100a,find(p.mce==0)))];
 
+else
+inda = p_ind.a;
+indb = p_ind.b;
+
+ind0 = [p_ind.a(ismember(p_ind.a,find(p.mce~=0))) ...
+    p_ind.b(ismember(p_ind.b,find(p.mce==0)))];
+ind90 = [p_ind.b(ismember(p_ind.b,find(p.mce~=0))) ...
+    p_ind.a(ismember(p_ind.a,find(p.mce==0)))];
+end
+
 len = length(scheds);
+
+%% Grab the phis per obs
 clc
 
 [phis, xpols, phis_err, xs, ys] = deal(NaN(len,2640));
@@ -343,9 +369,12 @@ for iterind = 1:Niter
     N(iterind) = length(find(~isnan(Vdiff)));
     idx(iterind,:) = ind1;
 end
+[~, mi] = max(abs(M.*sqrt(N)./S));
+idx_max = idx(mi,:);
+[~, mi] = min(abs(M.*sqrt(N)./S));
+idx_min = idx(mi,:);
 
-
-%% Figure 3.2.1 Consistency Checks Part 2 
+% Figure 3.2.1 Consistency Checks Part 2 
 
 clc
 V0 = {phi_pair; poleff_pair};
@@ -365,14 +394,14 @@ for pltind = 1:2%1:size(V,2)
     V1ttl = ttls{1};
     V2ttl = ttls{pltind};
     if pltind == 1
-        ind1 = [10 8 3 1 2];
+        ind1 = idx_min;
         ind2 = find(~ismember(1:10,ind1));
         V1 = V0{valind}(ind1,:);
         V2 = V{valind,pltind}(ind2,:);
         V1ttl = [V1ttl '\_SUB1'];
         V2ttl = [V2ttl '\_SUB2'];
     elseif pltind == 2
-        ind1 = [5 2 7 6 1];
+        ind1 = idx_max;
         ind2 = find(~ismember(1:10,ind1));
         V1 = V0{valind}(ind1,:);
         V2 = V{valind,pltind}(ind2,:);
@@ -444,17 +473,31 @@ end
 
 %% We need some extra stuff for the next section
 % Grab time, obs number, and phi_pair in the structure
-load('z:/dev/rps/sch_type5.mat')
-load('z:/dev/rps/rps_tilt_data_2022.mat')
+
+%load('z:/dev/rps/rps_tilt_data_2022.mat')
+
+%% Find the phis without tilt correction
+[fd.phi_notilt, fd.xpol_notilt] = deal(NaN(size(fd.ch)));
+for chind = 1:length(fd.ch)
+    ind = find(fd_notilt.schnum==fd.schnum(chind) & fd_notilt.rowind==fd.rowind(chind) &...
+        fd_notilt.ch==fd.ch(chind));
+    if ~isempty(ind)
+        fd.phi_notilt(chind) = nanmean(fd_notilt.phi(ind));
+        fd.xpol_notilt(chind) = nanmean(fd_notilt.xpol(ind));
+    end
+end
+
 
 %%
 clc
-[fd.t, fd.obsnum,fd.phi_pair, fd.poleff,fd.r,fd.theta,fd.xm,fd.ym,fd.thetam] = deal(NaN(size(fd.ch)));
+[fd.t, fd.obsnum,fd.phi_pair, fd.poleff,fd.r,fd.theta,...
+    fd.xm,fd.ym,fd.thetam,fd.phi_pair_corr,fd.phi_pair_notilt,...
+    fd.poleff_corr,fd.poleff_notilt] = deal(NaN(size(fd.ch)));
 for chind = 1:length(fd.ch)
     s = sch{fd.schnum(chind)};
     idx = s.index(fd.rowind(chind),1);
     fd.t(chind) = s.scans(idx).t1;
-
+    
     % Obs Number
     for schind = 1:length(scheds)
         if ismember(fd.schnum(chind),scheds{schind})
@@ -469,21 +512,31 @@ for chind = 1:length(fd.ch)
         if ~isempty(has90)
             [fd.phi_pair(chind) fd.poleff(chind)] = calc_pair_diff_pol(fd.phi(chind),...
                 nanmean(fd.phi(has90)),fd.xpol(chind),nanmean(fd.xpol(has90)));
+            [fd.phi_pair_corr(chind) fd.poleff_corr(chind)] = calc_pair_diff_pol(fd.phi_corr(chind),...
+                nanmean(fd.phi_corr(has90)),fd.xpol(chind),nanmean(fd.xpol(has90)));
+            [fd.phi_pair_notilt(chind) fd.poleff_notilt(chind)] = calc_pair_diff_pol(fd.phi_notilt(chind),...
+                nanmean(fd.phi_notilt(has90)),fd.xpol_notilt(chind),nanmean(fd.xpol_notilt(has90)));
         end
     end
-        
+
+    % Tilt info
+    if 0
+        idx2 = max(s.index(fd.rowind(chind),:));
+        fd.t2(chind) = s.scans(idx2).t2;
+        ind = lj_data.time >= fd.t(chind) & lj_data.time<=fd.t2(chind);
+        fd.tilt_out(chind) = nanmean(lj_data.AIN0(ind));
+        fd.tilt_out_std(chind) = nanstd(lj_data.AIN0(ind));
+        fd.tilt_temp(chind) = nanmean(lj_data.AIN2(ind));
+    end
 end
 
-% Tilt info
-fd.tilt_out = interp1(lj_data.time,lj_data.AIN0,fd.t);
-fd.tilt_temp = interp1(lj_data.time,lj_data.AIN2,fd.t);
 
 
-%% Mirror coords
-    [fd.theta, fd.r] = cart2pol(fd.x,fd.y);
-    fd.theta = wrapTo360(fd.theta*180/pi);
-    fd.thetam = wrapTo360(fd.theta-fd.dk_cen);
-    [fd.xm, fd.ym] = pol2cart(fd.thetam*pi/180,fd.r);
+% Mirror coords
+[fd.theta, fd.r] = cart2pol(fd.x,fd.y);
+fd.theta = wrapTo360(fd.theta*180/pi);
+fd.thetam = wrapTo360(fd.theta-fd.dk_cen);
+[fd.xm, fd.ym] = pol2cart(fd.thetam*pi/180,fd.r);
 
 %% Grab the moon-sun angles
 % MJD, , ,raapp,decapp
@@ -508,54 +561,100 @@ fclose(f);
 fd.az_cen_moon = interp1(hd_moon{1},unwrap(hd_moon{4}*pi/180)*180/pi,fd.t);
 fd.el_cen_moon = interp1(hd_moon{1},hd_moon{5},fd.t);
 
+
 %% phi vs. other stuff
 
 fd.az_cen = wrapTo360(fd.az_cen);
 fd.tod = fd.t-floor(fd.t);
+fd.xpol_corr = fd.xpol;
 
 clc
-Y = {'phi', 'phi','phi_pair';
-    'xpol','xpol','poleff'};
-yidx = {ismember(fd.ch,ind0), ismember(fd.ch,ind90), ismember(fd.ch,ind0)};
-ynames = {'phi_a','phi_b','phi_p';...
-    'xpol_a','xpol_b','xpol_p'};
-yttls = {'Pol A','Pol B','Pair-Diff'};
-ylims = {[-4.5 0], [-4.5 0]+90, [-4.5 0];...
-    [-1 1]*0.02,[-1 1]*0.02, [-1 10]*1e-4};
-ylims = {[-1 1]*1, [-1 1]*1, [-1 1]*0.5;...
-    [-1 1]*0.01,[-1 1]*0.01, [-1 1]*6e-4};
+corrnames = {'','_corr','_notilt'};
+for corrind = 2%1:length(corrnames)
+    Y = {'phi', 'phi','phi_pair';
+        'xpol','xpol','poleff'};
+    
+    yidx = {ismember(fd.ch,ind0), ismember(fd.ch,ind90), ismember(fd.ch,ind0)};
+    ynames = {'phi_a','phi_b','phi_p';...
+        'xpol_a','xpol_b','xpol_p'};
+    yttls = {'Pol A','Pol B','Pair-Diff'};
+    ylims = {[-4.5 0], [-4.5 0]+90, [-4.5 0];...
+        [-1 1]*0.02,[-1 1]*0.02, [-1 10]*1e-4};
+    ylims = {[-1 1]*1, [-1 1]*1, [-1 1]*0.5;...
+        [-1 1]*0.01,[-1 1]*0.01, [-1 1]*6e-4};
 
+    X = {'t','obsnum','az_cen','el_cen','dk_cen','az_cen_sun','el_cen_sun',...
+        'x','y','xm','ym','r','theta','thetam','tod'};
 
+    fig = figure(51);
+    fig.Position(3:4) = [900 400];
+    clf
 
-X = {'t','obsnum','az_cen','el_cen','dk_cen','az_cen_sun','el_cen_sun','x','y','xm','ym','r','theta','thetam','tilt_out','tilt_temp','tod'};
+    
+    for valind = 1:size(Y,1)
+        for yind = 1:size(Y,2)
+            Y{valind,yind} = [Y{valind,yind} corrnames{corrind}];
 
-fig = figure(51);
-fig.Position(3:4) = [900 400];
-clf
+            [medvals] = deal(NaN(1,2640));
+            for chind = 1:length(medvals)
+                ind = fd.ch==chind;
+                medvals(chind) = nanmedian(fd.(Y{valind,yind})(ind));
+            end
 
-medvals = {phis, phis,phi_pair;
-    xpols, xpols, poleff_pair};
-
-for valind = 1:size(Y,1)
-for yind = 1:size(Y,2)
-    medval = nanmedian(medvals{valind,yind},1);
-    for xind = 1:length(X)
-           if 0 
-        scatter(fd.(X{xind})(yidx{yind}),fd.(Y{valind,yind})(yidx{yind}),14,fd.obsnum(yidx{yind}),'filled')
-        ylim(ylims{valind,yind})
-       else
-        y = fd.(Y{valind,yind})(yidx{yind})-medval(fd.ch(yidx{yind}));
-        scatter(fd.(X{xind})(yidx{yind}),y,14,fd.dk_cen(yidx{yind}))%,'filled')
-        ylim(ylims{valind,yind})
-           end
-        colormap(cm)
-        grid on
-        xlabel(X{xind})
-        ylabel(Y{valind,yind})
-        fname = sprintf('scatter_%s_vs_%s.png',ynames{valind,yind},X{xind});
-        saveas(fig,fullfile(figdir,fname))
+            for xind = 1:length(X)
+                if 0
+                    scatter(fd.(X{xind})(yidx{yind}),fd.(Y{valind,yind})(yidx{yind}),14,fd.obsnum(yidx{yind}),'filled')
+                    ylim(ylims{valind,yind})
+                else
+                    y = fd.(Y{valind,yind})(yidx{yind})-medvals(fd.ch(yidx{yind}));
+                    scatter(fd.(X{xind})(yidx{yind}),y,14,fd.dk_cen(yidx{yind}))%,'filled')
+                    ylim(ylims{valind,yind})
+                end
+                colormap(cm)
+                grid on
+                xlabel(X{xind})
+                ylabel(Y{valind,yind})
+                fname = sprintf('scatter_%s_vs_%s%s.png',ynames{valind,yind},X{xind},corrnames{corrind});
+                saveas(fig,fullfile(figdir,fname))
+            end
+        end
     end
 end
+
+%% Compare phis to diff-pol corrected
+
+clc
+% grab medians for subtraction
+[medvals, medvals_corr] = deal(NaN(2640,1));
+[fd.phi_medsub, fd.phi_corr_medsub] = deal(NaN(size(fd.ch)));
+
+for chind = 1:length(medvals)
+    ind = fd.ch==chind;
+    medvals(chind) = nanmedian(fd.phi(ind));
+    medvals_corr(chind) = nanmedian(fd.phi_corr(ind));
+    fd.phi_medsub(ind) = fd.phi(ind)-medvals(chind);
+    fd.phi_corr_medsub(ind) = fd.phi_corr(ind)-medvals_corr(chind);
+end
+
+%
+
+fig = figure(1);
+clf;
+V = {fd.phi_medsub,fd.phi_corr_medsub};
+
+for pltind = 1:2
+subplot(1,2,pltind)
+edges = (-1:0.075:1)*0.6;
+N = histc(V{pltind},edges);
+b = bar(edges,N,'histc');
+b.FaceColor = cmlines(1,:);
+grid on
+xlabel('\phi-<\phi> [Degrees]')
+title({'No Pol Correction',...
+    sprintf('M: %0.4f | S: %0.4f | N: %i | EOM: %0.4f',...
+    nanmean(V{pltind}),nanstd(V{pltind}),length(find(~isnan(V{pltind}))),...
+    nanstd(V{pltind})./sqrt(length(find(~isnan(V{pltind})))))})
+xlim([edges([1 end])])
 end
 
 
